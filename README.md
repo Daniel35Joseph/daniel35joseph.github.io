f0292545f4d4cc01cc1e1c48cff079f1bda48a14
Daniel35Joseph <87067443+Daniel35Joseph@users.noreply.github.com>
2023-06-03 21:09:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Wordle-Game
+ It is a simple wordle game created using basic HTML, CSS, Javascript; with a help from Chat-GPT
---
+++ five_letters_words.js
@@ -0,0 +1,58 @@
+const fiveLetterWords = [
+    "APPLE",
+    "HOUSE",
+    "TABLE",
+    "BREAD",
+    "CHAIR",
+    "MUSIC",
+    "PLANT",
+    "RIVER",
+    "TEETH",
+    "BEACH",
+    "CLOUD",
+    "DANCE",
+    "EARTH",
+    "FAIRY",
+    "GHOST",
+    "HONEY",
+    "IMAGE",
+    "JELLY",
+    "KNEES",
+    "LEMON",
+    "MANGO",
+    "NOBLE",
+    "OCEAN",
+    "PIANO",
+    "QUEEN",
+    "RADIO",
+    "SUNNY",
+    "TIGER",
+    "UNCLE",
+    "VIRUS",
+    "WHEAT",
+    "XENON",
+    "YOUNG",
+    "ZEBRA"
+];
+
+async function loadFiveLetterWords() {
+    const response = await fetch('https://wordsapiv1.p.rapidapi.com/words/?letters=5', {
+      headers: {
+        'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com',
+        'X-RapidAPI-Key': 'YOUR_API_KEY'
+      }
+    });
+  
+    if (response.ok) {
+      const data = await response.json();
+      const words = data.results.data;
+      console.log(words);
+    } else {
+      console.error('Failed to fetch the words.');
+    }
+  }
+  
+loadFiveLetterWords();
+
+  
+  
